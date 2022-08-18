931102099d03017ea01ad043a3f5cc5283921293
王珠峰 <734765725@qq.com>
2022-08-18 13:49:46 +0800

    新版Dijkstra

--- practiceInterview/practiceInterview.go
+++ practiceInterview/practiceInterview.go
@@ -1,7 +1,6 @@
 package practiceinterview
 
 import (
-	"errors"
 	"sync"
 )
 
@@ -9,7 +8,9 @@ func Test(waitGroup *sync.WaitGroup) {
 	defer waitGroup.Done()
 
 	// 有向有权图最短路问题
-	// dijkstra()
+	// Dijkstra_Alg()
+
+	// // dijkstra()
 
 	// B+查找树
 	// BPlusTreeFunc()
@@ -34,6 +35,10 @@ func Test(waitGroup *sync.WaitGroup) {
 	// log.Println("堆排序优化后\r\n", time.Since(start_2))
 }
 
+func Dijkstra_Alg(graph [][]int, start int) {
+
+}
+
 /**
  * @description: 有向有权图最短路问题
  * @param {[][]int} graph 图抽象的邻接表(二维数组)
@@ -42,33 +47,33 @@ func Test(waitGroup *sync.WaitGroup) {
  * @return {[]int} 最短路线
  * @return {int} 最短路线距离
  */
-func dijkstra(graph [][]int, start, end int) ([]int, int) {
-	lenGraph := len(graph)       //节点数量
-	pre := make([]int, lenGraph) //记录前驱
-	// vis := make([]int, lenGraph) //记录节点遍历状态
-	dis := []int{} //保存最短距离
-	for i := 0; i < lenGraph; i++ {
-		dis = append(dis, i)
-	}
-	// road := make([]int, lenGraph) //保存最短路径
-	// roads := []int{}
+// func dijkstra(graph [][]int, start, end int) ([]int, int) {
+// 	lenGraph := len(graph)       //节点数量
+// 	pre := make([]int, lenGraph) //记录前驱
+// 	// vis := make([]int, lenGraph) //记录节点遍历状态
+// 	dis := []int{} //保存最短距离
+// 	for i := 0; i < lenGraph; i++ {
+// 		dis = append(dis, i)
+// 	}
+// 	// road := make([]int, lenGraph) //保存最短路径
+// 	// roads := []int{}
 
-	//初始化起点到其他点的距离
-	for i := 0; i < lenGraph; i++ {
-		if i == start {
-			dis[i] = 0
-		} else {
-			dis[i] = graph[start][i]
-		}
-		if graph[start][i] != 999 {
-			pre[i] = start
-		} else {
-			pre[i] = -1
-		}
-	}
+// 	//初始化起点到其他点的距离
+// 	for i := 0; i < lenGraph; i++ {
+// 		if i == start {
+// 			dis[i] = 0
+// 		} else {
+// 			dis[i] = graph[start][i]
+// 		}
+// 		if graph[start][i] != 999 {
+// 			pre[i] = start
+// 		} else {
+// 			pre[i] = -1
+// 		}
+// 	}
 
-	return []int{}, -1
-}
+// 	return []int{}, -1
+// }
 
 // 节点数据结构***************************************************************
 type BPlusItem struct {
@@ -114,33 +119,33 @@ func NewBPlusTree(width int) (BPlusTree *BPlusTree) {
  * @param {int} key   查询的值
  * @return {*}        结果
  */
-func (_BPlusTree *BPlusTree) get(key int) interface{} {
+// func (_BPlusTree *BPlusTree) get(key int) interface{} {
 
-	_BPlusTree.mutex.Lock()
-	defer _BPlusTree.mutex.Unlock()
+// 	_BPlusTree.mutex.Lock()
+// 	defer _BPlusTree.mutex.Unlock()
 
-	// 循环寻找所在节点
-	node := _BPlusTree.root
-	for i := 0; i < len(node.Nodes); i++ {
-		if key <= node.Nodes[i].MaxKey {
-			node = node.Nodes[i]
-			i = 0
-		}
-	}
-	// 没有到达叶子结点
-	if len(node.Nodes) > 0 {
-		return nil
-	}
+// 	// 循环寻找所在节点
+// 	node := _BPlusTree.root
+// 	for i := 0; i < len(node.Nodes); i++ {
+// 		if key <= node.Nodes[i].MaxKey {
+// 			node = node.Nodes[i]
+// 			i = 0
+// 		}
+// 	}
+// 	// 没有到达叶子结点
+// 	if len(node.Nodes) > 0 {
+// 		return nil
+// 	}
 
-	// 循环节点的阶(数据)
-	for i := 0; i < len(node.Items); i++ {
-		if node.Items[i].Key == key {
-			return node.Items[i].Val
-		}
-	}
+// 	// 循环节点的阶(数据)
+// 	for i := 0; i < len(node.Items); i++ {
+// 		if node.Items[i].Key == key {
+// 			return node.Items[i].Val
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 /**
  * @description: 节点添加数据处理函数
@@ -148,67 +153,67 @@ func (_BPlusTree *BPlusTree) get(key int) interface{} {
  * @param {interface{}} value
  * @return {*}
  */
-func (_BPlusNode *BPlusNode) setValue(key int, value interface{}) (ok bool, err error) {
+// func (_BPlusNode *BPlusNode) setValue(key int, value interface{}) (ok bool, err error) {
 
-	item := BPlusItem{key, value} //要插入的真实数据
-	num := len(_BPlusNode.Items)  //当前节点的有几条真实数据
+// 	item := BPlusItem{key, value} //要插入的真实数据
+// 	num := len(_BPlusNode.Items)  //当前节点的有几条真实数据
 
-	if num < 1 { //当前节点没有存储数据
-		_BPlusNode.Items = append(_BPlusNode.Items, item) //将数据加入当前节点存储
-		_BPlusNode.MaxKey = item.Key                      //当前节点最大值设为插入节点的key
-		return true, nil
-	} else if key < _BPlusNode.Items[0].Key { //设置的key值小于当前节点存储数据第一条,
-		_BPlusNode.Items = append([]BPlusItem{item}, _BPlusNode.Items...) //将数据插入到第一位
-		return true, nil
-	} else if key > _BPlusNode.Items[num-1].Key { //设置的key值大于当前节点存储数据第最后一条
-		_BPlusNode.Items = append(_BPlusNode.Items, item) //将数据插入到第最后一位
-		_BPlusNode.MaxKey = item.Key                      //当前节点最大值设置为插入的数据
-		return true, nil
-	}
+// 	if num < 1 { //当前节点没有存储数据
+// 		_BPlusNode.Items = append(_BPlusNode.Items, item) //将数据加入当前节点存储
+// 		_BPlusNode.MaxKey = item.Key                      //当前节点最大值设为插入节点的key
+// 		return true, nil
+// 	} else if key < _BPlusNode.Items[0].Key { //设置的key值小于当前节点存储数据第一条,
+// 		_BPlusNode.Items = append([]BPlusItem{item}, _BPlusNode.Items...) //将数据插入到第一位
+// 		return true, nil
+// 	} else if key > _BPlusNode.Items[num-1].Key { //设置的key值大于当前节点存储数据第最后一条
+// 		_BPlusNode.Items = append(_BPlusNode.Items, item) //将数据插入到第最后一位
+// 		_BPlusNode.MaxKey = item.Key                      //当前节点最大值设置为插入的数据
+// 		return true, nil
+// 	}
 
-	for i := 0; i < num; i++ { //如果数据大小在中间，循环插入
-		if _BPlusNode.Items[i].Key > key { //如果该节点key大于将要设置的值key
-			_BPlusNode.Items = append(_BPlusNode.Items, BPlusItem{}) //追加一个空数据
-			copy(_BPlusNode.Items[i+1:], _BPlusNode.Items[i:])       //整体数据后移一位
-			_BPlusNode.Items[i] = item                               // 插入数据
-			return true, nil
-		} else if _BPlusNode.Items[i].Key == key { //如果发现相同主键key
-			_BPlusNode.Items[i] = item //直接赋值
-			return true, nil
-		}
-	}
+// 	for i := 0; i < num; i++ { //如果数据大小在中间，循环插入
+// 		if _BPlusNode.Items[i].Key > key { //如果该节点key大于将要设置的值key
+// 			_BPlusNode.Items = append(_BPlusNode.Items, BPlusItem{}) //追加一个空数据
+// 			copy(_BPlusNode.Items[i+1:], _BPlusNode.Items[i:])       //整体数据后移一位
+// 			_BPlusNode.Items[i] = item                               // 插入数据
+// 			return true, nil
+// 		} else if _BPlusNode.Items[i].Key == key { //如果发现相同主键key
+// 			_BPlusNode.Items[i] = item //直接赋值
+// 			return true, nil
+// 		}
+// 	}
 
-	return false, errors.New("BPlusNode节点添加数据失败") //走到这里
-}
+// 	return false, errors.New("BPlusNode节点添加数据失败") //走到这里
+// }
 
 /**
  * @description:分割节点
  * @param {*BPlusNode} node
  * @return {*}
  */
-func (_BPlusTree *BPlusTree) splitNode(node *BPlusNode) *BPlusNode {
+// func (_BPlusTree *BPlusTree) splitNode(node *BPlusNode) *BPlusNode {
 
-	if len(node.Nodes) > _BPlusTree.width { //如果节点子节点数量大于B+树的阶值
+// 	if len(node.Nodes) > _BPlusTree.width { //如果节点子节点数量大于B+树的阶值
 
-		halfw := _BPlusTree.width/2 + 1 //中间阶数
-		node2 := &BPlusNode{
-			Items:  make([]BPlusItem, _BPlusTree.width),
-			Nodes:  node.Nodes[halfw:len(node.Nodes)],
-			MaxKey: node.Nodes[len(node.Nodes)-1].MaxKey,
-		}
+// 		halfw := _BPlusTree.width/2 + 1 //中间阶数
+// 		node2 := &BPlusNode{
+// 			Items:  make([]BPlusItem, _BPlusTree.width),
+// 			Nodes:  node.Nodes[halfw:len(node.Nodes)],
+// 			MaxKey: node.Nodes[len(node.Nodes)-1].MaxKey,
+// 		}
 
-		node.Nodes = node.Nodes[0:halfw]
-		node.MaxKey = node.Nodes[len(node.Nodes)-1].MaxKey
+// 		node.Nodes = node.Nodes[0:halfw]
+// 		node.MaxKey = node.Nodes[len(node.Nodes)-1].MaxKey
 
-		return node2
+// 		return node2
 
-	} else if len(node.Items) > _BPlusTree.width {
+// 	} else if len(node.Items) > _BPlusTree.width {
 
-		return nil
-	}
+// 		return nil
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 /**
  * @description: 删除节点
@@ -228,39 +233,39 @@ func (_BPlusTree *BPlusTree) Remove(key int) {
  * @param {int} key
  * @return {*}
  */
-func (_BPlusTree *BPlusTree) deleteItem(parent *BPlusNode, node *BPlusNode, key int) {
-	lenNode := len(node.Nodes)
-	for i := 0; i < lenNode; i++ { //递归寻找节点
-		if key <= node.Nodes[i].MaxKey {
-			_BPlusTree.deleteItem(node, node.Nodes[i], key)
-			break
-		}
-	}
+// func (_BPlusTree *BPlusTree) deleteItem(parent *BPlusNode, node *BPlusNode, key int) {
+// 	lenNode := len(node.Nodes)
+// 	for i := 0; i < lenNode; i++ { //递归寻找节点
+// 		if key <= node.Nodes[i].MaxKey {
+// 			_BPlusTree.deleteItem(node, node.Nodes[i], key)
+// 			break
+// 		}
+// 	}
 
-	if lenNode < 1 {
+// 	if lenNode < 1 {
 
-	}
+// 	}
 
-}
+// }
 
-func (_BPlusNode *BPlusNode) deleteItem(key int) bool {
-	lenItem := len(_BPlusNode.Items)
-	for i := 0; i < lenItem; i++ {
+// func (_BPlusNode *BPlusNode) deleteItem(key int) bool {
+// 	lenItem := len(_BPlusNode.Items)
+// 	for i := 0; i < lenItem; i++ {
 
-		if _BPlusNode.Items[i].Key > key {
-			return false
-		} else if _BPlusNode.Items[i].Key == key {
-			copy(_BPlusNode.Items[i:], _BPlusNode.Items[i+1:]) //前移删除数据
-			_BPlusNode.Items = _BPlusNode.Items[0 : len(_BPlusNode.Items)-1]
-			_BPlusNode.MaxKey = _BPlusNode.Items[len(_BPlusNode.Items)-1].Key
-			return true
-		}
+// 		if _BPlusNode.Items[i].Key > key {
+// 			return false
+// 		} else if _BPlusNode.Items[i].Key == key {
+// 			copy(_BPlusNode.Items[i:], _BPlusNode.Items[i+1:]) //前移删除数据
+// 			_BPlusNode.Items = _BPlusNode.Items[0 : len(_BPlusNode.Items)-1]
+// 			_BPlusNode.MaxKey = _BPlusNode.Items[len(_BPlusNode.Items)-1].Key
+// 			return true
+// 		}
 
-	}
+// 	}
 
-	return false
+// 	return false
 
-}
+// }
 
 /**
  * @description: 树设置值
@@ -270,20 +275,20 @@ func (_BPlusNode *BPlusNode) deleteItem(key int) bool {
  * @param {interface{}} value
  * @return {*}
  */
-func (_BPlusTree *BPlusTree) setValueTree(parent *BPlusNode, node *BPlusNode, key int, value interface{}) {
+// func (_BPlusTree *BPlusTree) setValueTree(parent *BPlusNode, node *BPlusNode, key int, value interface{}) {
 
-	for i := 0; i < len(node.Nodes); i++ { //循环所有子节点
-		if key <= node.Nodes[i].MaxKey || i == len(node.Nodes)-1 { //如果设置的值小于某个子节点的最大值，或者循环完了所有子节点
-			_BPlusTree.setValueTree(node, node.Nodes[i], key, value) //进行递归
-			break
-		}
-	}
+// 	for i := 0; i < len(node.Nodes); i++ { //循环所有子节点
+// 		if key <= node.Nodes[i].MaxKey || i == len(node.Nodes)-1 { //如果设置的值小于某个子节点的最大值，或者循环完了所有子节点
+// 			_BPlusTree.setValueTree(node, node.Nodes[i], key, value) //进行递归
+// 			break
+// 		}
+// 	}
 
-	if len(node.Nodes) < 1 { //如果节点没有子节点
-		node.setValue(key, value) //直接设置值
-	}
+// 	if len(node.Nodes) < 1 { //如果节点没有子节点
+// 		node.setValue(key, value) //直接设置值
+// 	}
 
-}
+// }
 
 /**
  * @description: 树结构体设置值
@@ -303,19 +308,19 @@ func BPlusTreeFunc() {
 
 }
 
-func shellSort(slice_arg []int) {
-	StepLength := len(slice_arg) / 2
-	for gap := StepLength; gap >= 1; gap = gap / 2 {
+// func shellSort(slice_arg []int) {
+// 	StepLength := len(slice_arg) / 2
+// 	for gap := StepLength; gap >= 1; gap = gap / 2 {
 
-		for rightIndex := gap; rightIndex < len(slice_arg); rightIndex++ {
-			for leftIndex := rightIndex - gap; leftIndex >= 0; leftIndex = leftIndex - gap {
-				if slice_arg[leftIndex] > slice_arg[leftIndex+gap] {
-					slice_arg[leftIndex], slice_arg[leftIndex+gap] = slice_arg[leftIndex+gap], slice_arg[leftIndex]
-				}
-			}
-		}
-	}
-}
+// 		for rightIndex := gap; rightIndex < len(slice_arg); rightIndex++ {
+// 			for leftIndex := rightIndex - gap; leftIndex >= 0; leftIndex = leftIndex - gap {
+// 				if slice_arg[leftIndex] > slice_arg[leftIndex+gap] {
+// 					slice_arg[leftIndex], slice_arg[leftIndex+gap] = slice_arg[leftIndex+gap], slice_arg[leftIndex]
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
 /**
  * @name:
@@ -323,18 +328,18 @@ func shellSort(slice_arg []int) {
  * @param {[]int} slice_arg  数组切片
  * @return {*}
  */
-func heapSort(slice_arg []int) {
+// func heapSort(slice_arg []int) {
 
-	for nonLeafNode := len(slice_arg)/2 - 1; nonLeafNode >= 0; nonLeafNode-- {
-		adjustHeap(slice_arg, nonLeafNode, len(slice_arg))
-	}
+// 	for nonLeafNode := len(slice_arg)/2 - 1; nonLeafNode >= 0; nonLeafNode-- {
+// 		adjustHeap(slice_arg, nonLeafNode, len(slice_arg))
+// 	}
 
-	for endIndex := len(slice_arg) - 1; endIndex > 0; endIndex-- {
-		slice_arg[endIndex], slice_arg[0] = slice_arg[0], slice_arg[endIndex]
-		adjustHeap(slice_arg, 0, endIndex)
-	}
+// 	for endIndex := len(slice_arg) - 1; endIndex > 0; endIndex-- {
+// 		slice_arg[endIndex], slice_arg[0] = slice_arg[0], slice_arg[endIndex]
+// 		adjustHeap(slice_arg, 0, endIndex)
+// 	}
 
-}
+// }
 
 /**
  * @name:
@@ -344,105 +349,105 @@ func heapSort(slice_arg []int) {
  * @param {int} sliceLength_arg 数组切片长度
  * @return {*}
  */
-func adjustHeap(slice_arg []int, index_arg, sliceLength_arg int) {
+// func adjustHeap(slice_arg []int, index_arg, sliceLength_arg int) {
 
-	temp := slice_arg[index_arg]
+// 	temp := slice_arg[index_arg]
 
-	for childIndex := 2*index_arg + 1; childIndex < sliceLength_arg; childIndex = 2*childIndex + 1 {
+// 	for childIndex := 2*index_arg + 1; childIndex < sliceLength_arg; childIndex = 2*childIndex + 1 {
 
-		if childIndex+1 < sliceLength_arg && slice_arg[childIndex] < slice_arg[childIndex+1] {
-			childIndex++ //指向右子节点
-		}
+// 		if childIndex+1 < sliceLength_arg && slice_arg[childIndex] < slice_arg[childIndex+1] {
+// 			childIndex++ //指向右子节点
+// 		}
 
-		if slice_arg[childIndex] > slice_arg[index_arg] {
-			slice_arg[index_arg] = slice_arg[childIndex] //子节点大于父节点、交换位置
-			index_arg = childIndex
-		} else {
-			break
-		}
+// 		if slice_arg[childIndex] > slice_arg[index_arg] {
+// 			slice_arg[index_arg] = slice_arg[childIndex] //子节点大于父节点、交换位置
+// 			index_arg = childIndex
+// 		} else {
+// 			break
+// 		}
 
-		slice_arg[index_arg] = temp
+// 		slice_arg[index_arg] = temp
 
-	}
+// 	}
 
-}
+// }
 
 // 优化方法1---------------------------------------------------------------------
-func pointerSplitMergeSort(slice_arg []int, startIndex_arg, endInde_arg int) {
+// func pointerSplitMergeSort(slice_arg []int, startIndex_arg, endInde_arg int) {
 
-	if endInde_arg-startIndex_arg < 2 {
-		return
-	}
+// 	if endInde_arg-startIndex_arg < 2 {
+// 		return
+// 	}
 
-	mideleIndex := (startIndex_arg + endInde_arg) >> 1
+// 	mideleIndex := (startIndex_arg + endInde_arg) >> 1
 
-	pointerSplitMergeSort(slice_arg, startIndex_arg, mideleIndex)
-	pointerSplitMergeSort(slice_arg, mideleIndex, endInde_arg)
+// 	pointerSplitMergeSort(slice_arg, startIndex_arg, mideleIndex)
+// 	pointerSplitMergeSort(slice_arg, mideleIndex, endInde_arg)
 
-	pointerSplitMerge(slice_arg, startIndex_arg, mideleIndex, endInde_arg)
+// 	pointerSplitMerge(slice_arg, startIndex_arg, mideleIndex, endInde_arg)
 
-}
+// }
 
-func pointerSplitMerge(slice_arg []int, startIndex_arg, mideleIndex_arg, endInde_arg int) {
+// func pointerSplitMerge(slice_arg []int, startIndex_arg, mideleIndex_arg, endInde_arg int) {
 
-	leftSliceLen := mideleIndex_arg - startIndex_arg
-	leftSlice := make([]int, leftSliceLen)
+// 	leftSliceLen := mideleIndex_arg - startIndex_arg
+// 	leftSlice := make([]int, leftSliceLen)
 
-	copy(leftSlice, slice_arg[startIndex_arg:mideleIndex_arg])
+// 	copy(leftSlice, slice_arg[startIndex_arg:mideleIndex_arg])
 
-	leftIndex, startIndex, rightIndex := 0, startIndex_arg, mideleIndex_arg
-	for leftIndex < leftSliceLen {
-		if rightIndex < endInde_arg && slice_arg[rightIndex] < leftSlice[leftIndex] {
-			slice_arg[startIndex] = slice_arg[rightIndex]
-			startIndex++
-			rightIndex++
-		} else {
-			slice_arg[startIndex] = leftSlice[leftIndex]
-			startIndex++
-			leftIndex++
-		}
-	}
+// 	leftIndex, startIndex, rightIndex := 0, startIndex_arg, mideleIndex_arg
+// 	for leftIndex < leftSliceLen {
+// 		if rightIndex < endInde_arg && slice_arg[rightIndex] < leftSlice[leftIndex] {
+// 			slice_arg[startIndex] = slice_arg[rightIndex]
+// 			startIndex++
+// 			rightIndex++
+// 		} else {
+// 			slice_arg[startIndex] = leftSlice[leftIndex]
+// 			startIndex++
+// 			leftIndex++
+// 		}
+// 	}
 
-}
+// }
 
 // 普通方法2-------------------------------------------------------------------------
 // 分割切片
 
-func segmentation(_tempSlice []int) []int {
-	if len(_tempSlice) < 2 {
-		return _tempSlice
-	}
-	// 找到中间下标
-	middleIndeex := len(_tempSlice) / 2
-	// 左半部分切片只剩两个
-	leftSlice := segmentation(_tempSlice[:middleIndeex])
-	// 右半部分切片只剩两个
-	rightSlice := segmentation(_tempSlice[middleIndeex:])
-	return mergeSlices(leftSlice, rightSlice)
-}
+// func segmentation(_tempSlice []int) []int {
+// 	if len(_tempSlice) < 2 {
+// 		return _tempSlice
+// 	}
+// 	// 找到中间下标
+// 	middleIndeex := len(_tempSlice) / 2
+// 	// 左半部分切片只剩两个
+// 	leftSlice := segmentation(_tempSlice[:middleIndeex])
+// 	// 右半部分切片只剩两个
+// 	rightSlice := segmentation(_tempSlice[middleIndeex:])
+// 	return mergeSlices(leftSlice, rightSlice)
+// }
 
 // 合并切片
-func mergeSlices(_leftSlice, _rightSlice []int) []int {
+// func mergeSlices(_leftSlice, _rightSlice []int) []int {
 
-	resultSlice := make([]int, 0)
-	leftIndex, rightIndex := 0, 0
-	leftLen, rightLen := len(_leftSlice), len(_rightSlice)
+// 	resultSlice := make([]int, 0)
+// 	leftIndex, rightIndex := 0, 0
+// 	leftLen, rightLen := len(_leftSlice), len(_rightSlice)
 
-	for leftIndex < leftLen && rightIndex < rightLen {
+// 	for leftIndex < leftLen && rightIndex < rightLen {
 
-		if _leftSlice[leftIndex] > _rightSlice[rightIndex] {
-			resultSlice = append(resultSlice, _rightSlice[rightIndex])
-			rightIndex++
+// 		if _leftSlice[leftIndex] > _rightSlice[rightIndex] {
+// 			resultSlice = append(resultSlice, _rightSlice[rightIndex])
+// 			rightIndex++
 
-		} else {
-			resultSlice = append(resultSlice, _leftSlice[leftIndex])
-			leftIndex++
+// 		} else {
+// 			resultSlice = append(resultSlice, _leftSlice[leftIndex])
+// 			leftIndex++
 
-		}
-	}
+// 		}
+// 	}
 
-	resultSlice = append(resultSlice, _rightSlice[rightIndex:]...)
-	resultSlice = append(resultSlice, _leftSlice[leftIndex:]...)
+// 	resultSlice = append(resultSlice, _rightSlice[rightIndex:]...)
+// 	resultSlice = append(resultSlice, _leftSlice[leftIndex:]...)
 
-	return resultSlice
-}
+// 	return resultSlice
+// }
